be6b7f98145e38f884221806314e65b3ddc978da
frostlily2k4 <frost.lily2k4@gmail.com>
2026-09-13 20:01:01 +0530

    Configure frontend for production backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
   async rewrites() {
     return [
-      { source: "/api/:path*", destination: "http://localhost:8000/api/:path*" },
+      {
+        source: "/api/:path*",
+        destination: `${
+          process.env.BACKEND_URL || "http://localhost:8000"
+        }/api/:path*`,
+      },
     ];
   },
 };
